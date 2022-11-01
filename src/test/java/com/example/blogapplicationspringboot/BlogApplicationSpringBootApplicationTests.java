0f50803f660698e071499840b440989693729cf2
Kr1s4an <krisivratza@gmail.com>
2022-11-01 11:12:47 +0200

    Post

---
+++ src/test/java/com/example/blogapplicationspringboot/BlogApplicationSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogapplicationspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
